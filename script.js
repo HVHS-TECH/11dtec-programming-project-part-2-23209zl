e0c2d5ce5dcc9caa761e5ddca8533dfe4117af01
23209zl <23209zl@hvhs.school.nz>
2025-06-17 09:49:12 +0000

    I made a change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormInput(){
     var message = "Welcome to Hoop Culture ";
     message = message + input_name.value;
-    message = message + ". You chose " + BasketballGear.value
+    message = message + ". You chose to buy " + BasketballGear.value
     welcomeMessage.innerHTML = message;
-}
+} 
