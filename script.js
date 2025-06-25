8e42d312ea3ccc53a06fd6e99a3d3892db1f73d2
23209zl <23209zl@hvhs.school.nz>
2025-06-25 06:14:51 +0000

    Jun 25, 2025, 6:14 AM

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-function getFormInput() {
+function getFormInput(){
     message = "Welcome to Hoop Culture ";
     message = message + input_name.value;
     welcomeMessage.innerHTML = message;
-}
+} 
 
 function getValueInput() {
-   question = "You chose to buy ";
-   question = question + input_value.value + " " +  BasketballGear.value;
-   summaryMessage.innerHTML = question;
+    const gearPrices = {
+        "Basketball Wristband": 2,
+        "Basketball Socks": 5,
+        "Basketball Headband": 10,
+        "Basketball Shorts": 15,
+        "Basketball Jersey": 30,
+        "Basketball Shoes": 50,
+    };
+
+     let question = "You chose to buy ";
+     let quantity = input_value.value;
+     let gear = BasketballGear.value;
+     let cost = gearPrices[gear] * quantity;
+
+     question = question + quantity + " " + gear + "(s).";
+     question = question + "The cost for this much gear you have selected is $" + cost + ".";
+   
+    summaryMessage.innerHTML = question;
 }
 
-function getInfo(){
-    information = "Hi we are Hoop Culture. We sell basketball gears. For more information please go to Basketball Gear page. Thankyou!";
-    infoMessage.innerHTML = information;
-}
+
